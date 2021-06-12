0bd5b6f11c579301a5725c8817c392c7558d6088
Vendeva <v190584@mail.ru>
2021-06-12 21:03:27 +0500

    create repository folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { PORT } = require("./config");
+const fs = require("fs");
+const { PORT, useLocalPath } = require("./config");
 const { apiSettings, apiBuilds } = require("./routers");
 const port = PORT || 8080;
 
@@ -9,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+fs.mkdir(useLocalPath, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Directory created successfully!");
+});
 
 // settings routes
 app.use("/api/settings", apiSettings);
